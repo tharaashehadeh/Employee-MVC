56a06d576383f367846d414226865714729cccd8
unknown <Tharashehadeh@gmail.com>
2024-05-07 23:34:25 +0300

    Employee-MVC

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var employees = context.Employees.ToList();
+            return View("Index",employees);
+        }
+    }
+}
---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext :DbContext
+    {     public DbSet<Employee> Employees { set; get; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=employee;trusted_connection=true; trustServerCertificate=true");
+        }
+    }
+}
